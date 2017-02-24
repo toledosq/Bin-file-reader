1fb908244d7b24baa73d99fb9ee7027ebd780dae
toledosq <smith_chris93@hotmail.com>
2017-02-23 22:20:22 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,6 +9,6 @@ public class Main {
 	// A program that will read a .bin file and output binary, hexadecimal, 6-bit unsigned int, and 16-bit unsigned int
 	Scanner keyboard = new Scanner(System.in);
         String fileName = keyboard.nextLine();
-        readBinaryFile.readFile(fileName);
+        ReadBinaryFile.readFile(fileName);
     }
 }
